5a362383a5c6be07a877dbfb14bce3a398eb8517
vladimir <abstractcloud9@gmail.com>
2019-08-07 20:45:51 +0300

    codo

---
+++ resources/views/default.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-8 mt-5">
+            <h2>CODO</h2>
+        </div>
+    </div>
+</div>
+@endsection
