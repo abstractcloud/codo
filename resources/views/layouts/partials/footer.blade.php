fdbf3458fcd30f108e1e7dbd64ba9cd56dee0e2b
midas <avanturestico@gmail.com>
2019-09-04 15:03:21 +0300

    front and infrastructure

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    © 2019 CODO IDE <span class="d-none d-sm-inline-block">- Crafted with <i class="mdi mdi-heart text-danger"></i> by PHP 18-5 TEAM</span>
+</footer>
---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,72 @@
+<div class="left side-menu">
+    <div class="slimscroll-menu" id="remove-scroll">
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu" id="side-menu">
+                <li class="menu-title">Main</li>
+                <li>
+                    <a href="index.html" class="waves-effect">
+                        <i class="ti-home"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="calendar.html" class="waves-effect">
+                        <i class="ti-calendar"></i>
+                        <span>My Projects</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0);" class="waves-effect">
+                        <i class="ti-email"></i>
+                        <span> Messages <span class="float-right menu-arrow">
+                                        <i class="mdi mdi-chevron-right"></i></span>
+                                    </span>
+                    </a>
+
+                    <ul class="submenu">
+                        <li><a href="email-inbox.html">Inbox</a></li>
+                        <li><a href="email-read.html">Email Read</a></li>
+                        <li><a href="email-compose.html">Email Compose</a></li>
+                    </ul>
+
+                </li>
+
+
+                <li class="menu-title">Components</li>
+                <li>
+                    <a href="javascript:void(0);" class="waves-effect">
+                        <i class="ti-package"></i>
+                        <span>UI Elements <span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span></span>
+                    </a>
+
+                    <ul class="submenu">
+                        <li><a href="ui-alerts.html">Alerts</a></li>
+                        <li><a href="ui-buttons.html">Buttons</a></li>
+                        <li><a href="ui-cards.html">Cards</a></li>
+                        <li><a href="ui-carousel.html">Carousel</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0);" class="waves-effect">
+                        <i class="ti-receipt"></i><span> Forms <span class="badge badge-pill badge-success float-right">9</span></span>
+                    </a>
+
+                    <ul class="submenu">
+                        <li><a href="form-elements.html">Form Elements</a></li>
+                        <li><a href="form-validation.html">Form Validation</a></li>
+                        <li><a href="form-advanced.html">Form Advanced</a></li>
+                        <li><a href="form-editors.html">Form Editors</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- Sidebar -->
+
+        <div class="clearfix"></div>
+    </div><!-- Sidebar -left -->
+</div>
